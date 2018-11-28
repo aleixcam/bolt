935edbc48fd4ebc06f67800230b4c2669f73684c
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-11-28 18:57:59 +0100

    bugfix 2

--- public/frozenenv.js
+++ public/frozenenv.js
@@ -5,4 +5,8 @@ const isDev = require('electron-is-dev')
 
 const appPath = os.platform() === 'win32' ? 'resources\\app.asar' : app.getAppPath()
 process.env.LILLI_MODEL_DIRECTORY = isDev ? 'public/model' : path.join(appPath, 'build/model')
-if (!isDev) process.env.LILLI_DATA_DIRECTORY = path.join(app.getPath('appData'), 'bolt')
+
+if (!isDev) {
+    const dataPath = os.platform() === 'win32' ? 'resources' : app.getPath('appData')
+    process.env.LILLI_DATA_DIRECTORY = path.join(dataPath, 'bolt')
+}
