9a5f9325dbd42522a6e9bf351e17e9d93a461c2a
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-11-29 13:25:46 +0100

    bugfix

--- src/component/Alert.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { ToastContainer } from 'react-toastify';
-import Sidenav from './Sidenav'
-
-function Alert(props) {
-    return <ToastContainer />
-}
-
-export default Alert
